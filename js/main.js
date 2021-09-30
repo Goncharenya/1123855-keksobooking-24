ae44bd24a7406cfeb8fed3c044da3b96d0db8a66
Goncharenya <kotmay@mail.ru>
2021-09-30 22:20:47 +0300

    module2-task1

--- js/main.js
+++ js/main.js
@@ -16,3 +16,15 @@ const getRandomFloat = function (from, to, numOfDecimalPlaces) {
 };
 
 getRandomFloat(1, 100, 2);
+
+const getRandomNumUniversal = function (from, to, numOfDecimalPlaces) {
+  if (from < 0 || to <= from) {
+    throw new Error('некорректный диапазон чисел');
+  }
+  if (numOfDecimalPlaces >= 0) {
+    const randomNum = from + Math.random() * (to - from);
+    return randomNum.toFixed(numOfDecimalPlaces);
+  }
+};
+
+getRandomNumUniversal(1, 80, 0);
