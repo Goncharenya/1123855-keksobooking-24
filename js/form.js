8cd14654eb8d989d27a2f21413eb0a73ee7ab56f
Goncharenya <kotmay@mail.ru>
2021-10-29 06:53:41 +0300

    task-2

--- js/form.js
+++ js/form.js
@@ -2,8 +2,8 @@
 
 const adForm = document.querySelector('.ad-form');
 const formFilters = document.querySelector('.map__filters');
-const fieldsetInput = formFilters.querySelectorAll('input');
-const fieldsetOption = formFilters.querySelectorAll('option');
+const fieldsetInput = formFilters.children;
+//const fieldsetOption = formFilters.children;
 
 export const formDisable = () => {
   adForm.classList.add('ad-form--disabled');
@@ -12,9 +12,9 @@ export const formDisable = () => {
     elem.setAttribute('disabled', 'disabled');
   });
 
-  fieldsetOption.forEach((elem) => {
-    elem.setAttribute('disabled', 'disabled');
-  });
+  // fieldsetOption.forEach((elem) => {
+  //   elem.setAttribute('disabled', 'disabled');
+  // });
 };
 
 formDisable();
@@ -22,12 +22,13 @@ formDisable();
 export const formActive = () => {
   adForm.classList.remove('ad-form--disabled');
   formFilters.classList.remove('ad-form--disabled');
+
   fieldsetInput.forEach((elem) => {
     elem.removeAttribute('disabled');
   });
-  fieldsetOption.forEach((elem) => {
-    elem.removeAttribute('disabled');
-  });
+  // fieldsetOption.forEach((elem) => {
+  //   elem.removeAttribute('disabled');
+  // });
 };
 
 formActive();
